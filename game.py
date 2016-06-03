db85d89db204fb773d10dea615ded50fe2d98afa
Rajeev Singh <rajeevsinxh786@gmail.com>
2016-06-03 19:28:33 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,184 @@
+from tkinter import *
+import time
+import random
+
+root = Tk()
+root.title("Bounce")
+root.geometry("500x570")
+root.resizable(0, 0)
+root.wm_attributes("-topmost", 1)
+canvas = Canvas(root, width=500, height=500, bd=0, highlightthickness=0, highlightbackground="Red", bg="Black")
+canvas.pack(padx=10, pady=10)
+score = Label(height=50, width=80, text="Score: 00", font="Consolas 14 bold")
+score.pack(side="left")
+root.update()
+
+
+class Ball:
+    def __init__(self, canvas, color, paddle, bricks, score):
+        self.bricks = bricks
+        self.canvas = canvas
+        self.paddle = paddle
+        self.score = score
+        self.bottom_hit = False
+        self.hit = 0
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color, width=1)
+        self.canvas.move(self.id, 230, 461)
+        start = [3, 2.8, 2.6, 2.4, 2.2, 2, 1.8, 1.6]
+        random.shuffle(start)
+        self.x = start[0]
+        self.y = -start[0]
+        self.canvas.move(self.id, self.x, self.y)
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+    def brick_hit(self, pos):
+        for brick_line in self.bricks:
+            for brick in brick_line:
+                brick_pos = self.canvas.coords(brick.id)
+                # print(brick_pos)
+                try:
+                    if pos[2] >= brick_pos[0] and pos[0] <= brick_pos[2]:
+                        if pos[3] >= brick_pos[1] and pos[1] <= brick_pos[3]:
+                            self.hit += 1
+                            self.score.configure(text="Score: " + str(self.hit))
+                            self.canvas.delete(brick.id)
+                            return True
+                except:
+                    continue
+        return False
+
+    def paddle_hit(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[1] <= paddle_pos[3]:
+                # print("paddle hit")
+                return True
+            return False
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        # print(pos)
+        start = [3, 2.8, 2.6, 2.4, 2.2, 2, 1.8, 1.6]
+        random.shuffle(start)
+        if self.brick_hit(pos):
+            self.y = start[0]
+        if pos[1] <= 0:
+            self.y = start[0]
+        if pos[3] >= self.canvas_height:
+            self.bottom_hit = True
+        if pos[0] <= 0:
+            self.x = start[0]
+        if pos[2] >= self.canvas_width:
+            self.x = -start[0]
+        if self.paddle_hit(pos):
+            self.y = -start[0]
+
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = self.canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 485)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all("<Left>", self.turn_left)
+        self.canvas.bind_all("<Right>", self.turn_right)
+
+    def draw(self):
+        pos = self.canvas.coords(self.id)
+        # print(pos)
+        if pos[0] + self.x <= 0:
+            self.x = 0
+        if pos[2] + self.x >= self.canvas_width:
+            self.x = 0
+        self.canvas.move(self.id, self.x, 0)
+
+    def turn_left(self, event):
+        self.x = -3
+        '''while self.x < 0:
+            pos = self.canvas.coords(self.id)
+            if pos[0] + self.x <= 0:
+                self.x = 0
+            if pos[2]+ self.x >= self.canvas_width:
+                self.x = 0
+            self.canvas.move(self.id, self.x, 0)
+            root.update()
+            self.x += 0.01
+            time.sleep(0.005)
+        '''
+
+    def turn_right(self, event):
+        self.x = 3
+        '''while self.x > 0:
+            pos = self.canvas.coords(self.id)
+            if pos[0] + self.x <= 0:
+                self.x = 0
+            if pos[2]+ self.x >= self.canvas_width:
+                self.x = 0
+            self.canvas.move(self.id, self.x, 0)
+            root.update()
+            self.x -= 0.01
+            time.sleep(0.005)
+        '''
+
+
+class Bricks:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = self.canvas.create_oval(5, 5, 25, 25, fill=color, width=2)
+
+
+playing = False
+
+
+def start_game(event):
+    global playing
+    if playing is False:
+        playing = True
+        score.configure(text="Score: 00")
+        canvas.delete("all")
+        BALL_COLOR = ["red", "yellow", "white"]
+        BRICK_COLOR = ["PeachPuff3", "dark slate gray", "rosy brown", "light goldenrod yellow", "turquoise3", "salmon",
+                       "light steel blue", "dark khaki", "pale violet red", "orchid", "tan", "MistyRose2",
+                       "DodgerBlue4", "wheat2", "RosyBrown2", "bisque3", "DarkSeaGreen1"]
+        random.shuffle(BALL_COLOR)
+        paddle = Paddle(canvas, "blue")
+        bricks = []
+        for i in range(0, 5):
+            b = []
+            for j in range(0, 19):
+                random.shuffle(BRICK_COLOR)
+                tmp = Bricks(canvas, BRICK_COLOR[0])
+                b.append(tmp)
+            bricks.append(b)
+
+        for i in range(0, 5):
+            for j in range(0, 19):
+                canvas.move(bricks[i][j].id, 25 * j, 25 * i)
+
+        ball = Ball(canvas, BALL_COLOR[0], paddle, bricks, score)
+        root.update_idletasks()
+        root.update()
+
+        time.sleep(1)
+        while 1:
+            if not ball.bottom_hit:
+                ball.draw()
+                paddle.draw()
+                root.update_idletasks()
+                root.update()
+                time.sleep(0.01)
+            else:
+                canvas.create_text(250, 250, text="GAME OVER!!", fill="red", font="Consolas 24 ")
+                root.update_idletasks()
+                root.update()
+                playing = False
+                break
+
+
+root.bind_all("<Return>", start_game)
+# root.bind_all("<Up>", play)
+canvas.create_text(250, 250, text="Press Enter to start Game!!", fill="red", font="Consolas 18 ")
+root.mainloop()

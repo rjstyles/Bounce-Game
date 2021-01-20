2fd087878f0a295a11df67c13bc3c9d4535b4d44
audtjs0101 <audtjs0101@naver.com>
2021-01-20 18:17:38 +0900

    이미지 추가

--- resourceManager.py
+++ resourceManager.py
@@ -1,6 +1,9 @@
 # 리소스 파일들의 주소를 관리
 # 각 클래스는 이 파일을 import하여 사용
 
-bgImage = "bg.png"
+bgImage = "pixel 배경.jpg"
+mainball="달.png"
+bubble="pixel-soap.png"
 font = "Consolas 14 bold"
 bgMusic = "etude10-4.mp3"
+bar="Orchid"

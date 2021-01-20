5ffa823807941f8a46ddfefdda6279ddaccc2291
leedoyeon <gcdalee@gmail.com>
2021-01-20 13:49:22 +0900

    랭크 보여주는 함수 추가

--- rankdb.py
+++ rankdb.py
@@ -30,4 +30,30 @@ def rank(score):
 
 #랭크를 보여주는 함수.
 def showRank():
-    pass
+    root = Tk()
+    root.title("순위표")
+    root.geometry("300x400")
+    sql = "SELECT * FROM rank order by score DESC"
+    res = c.execute(sql)
+    treelist=[]
+    for r in res:
+        treelist.append(r)
+
+    treeview = ttk.Treeview(root, columns=["이름", "점수"], displaycolumns=["이름", "점수"])
+    treeview.pack()
+
+    treeview.column("#0", width=50, anchor="center")
+    treeview.heading("#0", text="순위", anchor="center")
+
+    treeview.column("이름", width=100, anchor="center")
+    treeview.heading("이름", text="이름", anchor="center")
+
+    treeview.column("점수", width=100, anchor="center")
+    treeview.heading("점수", text="점수", anchor="center")
+
+    for i in range(len(treelist)):
+        treeview.insert('', 'end', text=i+1, values=treelist[i])
+
+    treeview.pack()
+
+    root.mainloop()
